58a912779727a7f42de4bb61c5d6bc26c33f7231
vAnonym <106328886+viktorsynek@users.noreply.github.com>
2022-05-27 14:35:20 +0200

    random password generator - saving result to a txt

---
+++ main.py
@@ -0,0 +1,40 @@
+#variable assignment + imports
+from random import randint
+import string
+lst = []
+asd = 10
+password = ''
+
+# alphabet list
+alphabet_string =string.ascii_lowercase + string.ascii_uppercase 
+alphabet_list = list(alphabet_string)
+
+# number list
+numerical_string = list(range(48))
+numerical_list = []
+for i in numerical_string:
+    numerical_list.append(str(i))
+
+
+# opening + writing into file
+for i in range(20):
+    x = randint(0,47)
+    passwordHashA = alphabet_list[x]
+    passwordHashN = numerical_list[x]
+    passwordHash = passwordHashN + passwordHashA
+    password += passwordHash
+    asd -= 1
+    
+with open('password.txt', 'w+') as f:
+    f.write(password)
+
+# opening + reading the file
+with open('password.txt', 'r') as f:
+    x = f.readlines()
+    print(x)
+
+
+
+
+
+
