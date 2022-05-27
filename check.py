8b9fa23794f92f1267890e9381542f0d06185d91
vAnonym <106328886+viktorsynek@users.noreply.github.com>
2022-05-27 14:36:20 +0200

    password checker - length + has number in it

---
+++ check.py
@@ -0,0 +1,15 @@
+contains_number = False
+numbers = ['1','2','3','4','5','6','7','8','9']
+
+pw = input('Enter your password: ')
+
+for i in pw:
+    if i in numbers:
+        contains_number = True
+        break
+
+if len(pw) >= 8 and contains_number == True:
+    print('Password valid')
+
+else:
+    print('Password invalid!')
